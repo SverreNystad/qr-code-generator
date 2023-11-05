3c5e5681b8c2f28062ef08f22f614cce1add1f03
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2023-11-05 04:12:54 +0100

    Feat: Create qr_app.py

---
+++ qr_app.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from urllib.parse import urlparse
+from PIL import ImageTk, Image
+import os
+from qr_generator import generate_qr
+
+class QRCodeApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("QR Code Generator")
+        self.geometry("400x400")
+
+        tk.Label(self, text="Enter URL:").pack(pady=10)
+        self.url_entry = tk.Entry(self, width=40)
+        self.url_entry.pack(pady=10)
+
+        self.generate_button = tk.Button(self, text="Generate QR Code", command=self.generate)
+        self.generate_button.pack(pady=10)
+
+        self.save_button = tk.Button(self, text="Save QR Code", command=self.save, state=tk.DISABLED)
+        self.save_button.pack(pady=10)
+
+        self.canvas = tk.Canvas(self, width=200, height=200)
+        self.canvas.pack(pady=10)
+
+    def generate(self):
+        url = self.url_entry.get()
+        try:
+            self.img = generate_qr(url)
+            self.update_qr_code(self.img)
+            self.save_button.config(state=tk.NORMAL)
+        except ValueError as e:
+            tk.messagebox.showerror("Error", str(e))
+
+    def save(self):
+        default_dir = os.path.expanduser('~\\Downloads')
+        file_path = filedialog.asksaveasfilename(initialdir=default_dir, title="Save QR Code",
+                                                 filetypes=(("PNG files", "*.png"), ("All files", "*.*")),
+                                                 defaultextension=".png")
+        if file_path:
+            self.img.save(file_path)
+
+    def update_qr_code(self, img):
+        self.tk_img = ImageTk.PhotoImage(img)
+        self.canvas.config(width=self.tk_img.width(), height=self.tk_img.height())
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.tk_img)
+
+if __name__ == "__main__":
+    app = QRCodeApp()
+    app.mainloop()
